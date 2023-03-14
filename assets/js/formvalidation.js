30b02fb278ae498c396a92d00981675d87e6948d
antonio de gou <thestockofantonio@gmail.com>
2023-03-14 21:46:50 +0000

    added functionality to spot colours

--- assets/js/formvalidation.js
+++ assets/js/formvalidation.js
@@ -16,96 +16,225 @@ for(let i = 0; i < keys.length; i++) {
    select.appendChild(el);
 }
 
-console.log(keys.includes('blue'))
+// console.log(keys.includes('blue'))
 
 /* __________
 -- COLOUR CONVERTER FORM VALIDATION and CONVERTER AS TYPED --
 __________ */ 
 
 
-const submit = document.getElementById("submit")
+
 
 // handles and validates all input fom converter section 
 let converterInputs = document.querySelectorAll(".converter");
 
+const submit = document.getElementById("submit")
 const hex = document.getElementById('hex_code');
 const rgb = document.getElementById('rgb');
 const hsl = document.getElementById('hsl');
 const cssOption = document.getElementById('CSScolours');
 const colorPicker = document.getElementById('mypick');
 
+// converterInputs.forEach(function(elem) {
+
+//   if(elem.id == 'hex_code' || elem.id == 'rgb' || elem.id == 'hsl' ){
+
+//     elem.addEventListener('keyup', function(e){
+//       e.preventDefault();
+//       let input =  e.target;
+//       let parseInput = input.value.split(',').map(Number)
+
+//       if(input.id == 'hex_code'){
+//         const isValid =  input.validity.valid
+//         eventFunc(isValid, input, "Use valid Hex colour")
+//         rgb.value = convert.hex.rgb(input.value).toString()
+//         hsl.value = convert.hex.hsl(input.value).toString()
+//         colorPicker.value = "#"+input.value
+//         cssOption.value = convert.hex.keyword(input.value)
+//         validate([rgb,hsl,cssOption])
+//       }
+//       if(input.id == 'rgb'){
+//         let isValid = e.target.validity.valid
+//         eventFunc(isValid, input, "0-255, 0-255, 0-255")
+
+//         hex.value = convert.rgb.hex(parseInput)
+//         hsl.value = convert.rgb.hsl(parseInput)
+//         colorPicker.value = "#"+hex.value
+//         cssOption.value = convert.rgb.keyword(parseInput)
+//         validate([hex,hsl,cssOption])
+//       }
+//       if(input.id == 'hsl'){
+//         const isValid =  checkHSL(parseInput)
+//         eventFunc(isValid, input, "0-360, 0-100, 0-100")
+
+//         hex.value = convert.hsl.hex(parseInput)
+//         rgb.value = convert.hsl.rgb(parseInput)
+//         colorPicker.value = "#"+hex.value
+//         cssOption.value = convert.hsl.keyword(parseInput)
+//         validate([hex,rgb,cssOption])
+//       }
+//     });
+//   } 
+//   if(elem.id == 'CSScolours' || elem.id == 'mypick'  ){
+//     elem.addEventListener('input', function(e){
+//       e.preventDefault();
+//       let input =  e.target;
+//       if(input.id == 'CSScolours'){
+
+//         const isValid =  keys.includes(input.value)
+//         eventFunc(isValid, input, "not a css colour")
+//         if(isValid){
+//           hex.value = convert.keyword.hex(input.value.toString()) 
+//           rgb.value = convert.keyword.rgb(input.value.toString()) 
+//           hsl.value = convert.keyword.hsl(input.value.toString()) 
+//           colorPicker.value = "#"+hex.value
+
+//           validate([hsl,hex,rgb])
+//         }
+//       }
+//       if(input.id == 'mypick'){
+
+//           hex.value = input.value.replace('#', '');
+//           rgb.value = convert.hex.rgb(hex.value).toString()
+//           hsl.value = convert.hex.hsl(hex.value).toString()
+//           cssOption.value = convert.hex.keyword(hex.value)
+//           submit.removeAttribute('disabled')
+//           validate([hsl,hex,rgb,cssOption])
+//       }
+
+//     })
+//   }
+
+// });
+
+// console.log(converterInputs)
+
 converterInputs.forEach(function(elem) {
-
   if(elem.id == 'hex_code' || elem.id == 'rgb' || elem.id == 'hsl' ){
-
     elem.addEventListener('keyup', function(e){
       e.preventDefault();
       let input =  e.target;
-      let parseInput = input.value.split(',').map(Number)
-
+      submit.setAttribute("disabled", "");
       if(input.id == 'hex_code'){
-        const isValid =  input.validity.valid
-        eventFunc(isValid, input, "Use valid Hex colour")
-        rgb.value = convert.hex.rgb(input.value).toString()
-        hsl.value = convert.hex.hsl(input.value).toString()
-        colorPicker.value = "#"+input.value
-        cssOption.value = convert.hex.keyword(input.value)
+        
+        converterColours([input])
+        validateColours([input])
+        converterColours(['rgb', 'hsl','CSScolours', 'myPick' ])
+      
+        // validateColours([rgb , hsl , cssOption ])
+      
       }
       if(input.id == 'rgb'){
-        let isValid = e.target.validity.valid
-        eventFunc(isValid, input, "0-255, 0-255, 0-255")
-
-        hex.value = convert.rgb.hex(parseInput)
-        hsl.value = convert.rgb.hsl(parseInput)
-        colorPicker.value = "#"+hex.value
-        cssOption.value = convert.rgb.keyword(parseInput)
+        converterColours([input])
+        validateColours([input])
+        converterColours(['hex_code', 'hsl', 'CSScolours', 'myPick' ])
+        validateColours([hex_code, hsl, cssOption ])
       }
       if(input.id == 'hsl'){
-        const isValid =  checkHSL(parseInput)
-        eventFunc(isValid, input, "0-360, 0-100, 0-100")
-
-        hex.value = convert.hsl.hex(parseInput)
-        rgb.value = convert.hsl.rgb(parseInput)
-        colorPicker.value = "#"+hex.value
-        cssOption.value = convert.hsl.keyword(parseInput)
+        converterColours([input])
+        validateColours([input])
+        converterColours(['hex_code', 'rgb','CSScolours', 'myPick' ])
+        validateColours([hex_code, rgb, cssOption ])
       }
-    });
-  } 
+    })
+  }
   if(elem.id == 'CSScolours' || elem.id == 'mypick'  ){
-    elem.addEventListener('input', function(e){
+    elem.addEventListener('change', function(e){
       e.preventDefault();
       let input =  e.target;
-      if(input.id == 'CSScolours'){
 
-        const isValid =  keys.includes(input.value)
-        eventFunc(isValid, input, "not a css colour")
-        if(isValid){
-          hex.value = convert.keyword.hex(input.value.toString()) 
-          rgb.value = convert.keyword.rgb(input.value.toString()) 
-          hsl.value = convert.keyword.hsl(input.value.toString()) 
-          colorPicker.value = "#"+hex.value
-        }
+      if(input.id == 'CSScolours'){
+        converterColours([input])
+        validateColours([input])
+        converterColours(['hex_code', 'rgb', 'hsl', 'myPick' ])
+        validateColours([hex_code, rgb, hsl])
       }
       if(input.id == 'mypick'){
-
-          hex.value = input.value.replace('#', '');
-          rgb.value = convert.hex.rgb(hex.value).toString()
-          hsl.value = convert.hex.hsl(hex.value).toString()
-          cssOption.value = convert.hex.keyword(hex.value)
-          submit.removeAttribute('disabled')
-
+        converterColours([input])
+        converterColours(['hex_code', 'rgb', 'hsl', 'CSScolours' ])
+        validateColours([hex_code, rgb, hsl, cssOption ])
       }
-
     })
   }
 
-});
+})
 
 
 
+const converterColours = (inputs) => {
+
+  inputs.forEach((input) => {
+    
+    if(input.id == 'hex_code'){
+      rgb.value = convert.hex.rgb(input.value).toString()
+      hsl.value = convert.hex.hsl(input.value).toString()
+      // colorPicker.value = "#"+input.value
+      cssOption.value = convert.hex.keyword(input.value)
+    }
+    if(input.id == 'rgb'){
+      let parseInput = input.value.split(',').map(Number)
+      hex.value = convert.rgb.hex(parseInput)
+      hsl.value = convert.rgb.hsl(parseInput)
+      colorPicker.value = "#"+hex.value
+      cssOption.value = convert.rgb.keyword(parseInput)
+    }
+
+    if(input.id == 'hsl'){
+      let parseInput = input.value.split(',').map(Number)
+      hex.value = convert.hsl.hex(parseInput)
+      rgb.value = convert.hsl.rgb(parseInput)
+      colorPicker.value = "#"+hex.value
+      cssOption.value = convert.hsl.keyword(parseInput)
+    }
+    if(input.id == 'CSScolours'){
+      hex.value = convert.keyword.hex(input.value.toString()) 
+      rgb.value = convert.keyword.rgb(input.value.toString()) 
+      hsl.value = convert.keyword.hsl(input.value.toString()) 
+      colorPicker.value = "#"+hex.value
+    }
+    if(input.id == 'mypick'){
+      hex.value = input.value.replace('#', '');
+      rgb.value = convert.hex.rgb(hex.value).toString()
+      hsl.value = convert.hex.hsl(hex.value).toString()
+      cssOption.value = convert.hex.keyword(hex.value)
+    }
+  })
+}
+
+const validateColours = (validateArray) => {
+  validateArray.forEach((ele)=>{
+    let isValid;
+    if(ele.id == 'hex_code' ){
+      isValid =  ele.validity.valid
+      // console.log(ele.validity )
+      eventFunc(isValid, ele , "Use valid Hex colour")
+    }
+    if(ele.id == 'rgb'){
+      isValid =  ele.validity.valid
+      eventFunc(isValid, ele, "0-255, 0-255, 0-255")
+    }
+    if(ele.id == 'hsl'){
+      let parseInput = ele.value.split(',').map(Number)
+      isValid = checkHSL(parseInput)
+      eventFunc(isValid, ele, "0-360, 0-100, 0-100")
+    }
+    if(ele.id == 'CSScolours'){
+      isValid =  keys.includes(ele.value)
+      eventFunc(isValid, ele, "not a css colour")
+    }
+
+  })
+}
 
 const eventFunc = (isValid, input, invalidMessage) =>{
+  // console.log("a " + isValid)
   if(!isValid) {
+    // if(input.id == 'hex_code'){
+    //  submit.setAttribute("disabled", "")   
+    // }
+    // input.setCustomValidity(invalidMessage);
+    input.reportValidity()
+    submit.setAttribute("disabled", "");
     input.classList.add('invalid');
     input.classList.remove('valid'); 
     if(input.id == 'hsl'){
@@ -113,8 +242,10 @@ const eventFunc = (isValid, input, invalidMessage) =>{
       input.setAttribute("isvalid", "false")
     }
     input.reportValidity()
-    submit.setAttribute("disabled", "");
+
+  
   }else{
+    input.setCustomValidity("");
     input.classList.remove('invalid');
     input.classList.add('valid');
     if(input.id == 'hsl'){
--- main.js
+++ main.js
@@ -8,5 +8,50 @@ import './style.scss'
 // import cssKeyword from "color-name";
 import * as cosmetic from '/assets/js/cosmetic.js'
 import * as formValidation from '/assets/js/formvalidation.js'
+import { Gradient } from "javascript-color-gradient";
+
+document.getElementById("currentYear").innerHTML = new Date().getFullYear();
+
+
+let spotColList = []
+const submit = document.getElementById("submit")
+const spotCols = document.getElementById('spotcols')
+const spotPrev = document.getElementById('gradpreview')
+
+submit.addEventListener('click', function(e){
+  e.preventDefault()
+  let hex = document.getElementById('hex_code');
+  spotColList.push(hex.value)
+ let a = ''
+ let b= ''
+  spotColList.forEach(item => {
+    a += `
+    <div class="row spot-col mb-3">
+      <div class="col-2 remove d-flex align-items-center justify-content-center">
+        <span class="icon-document-delete"></span>
+      </div>
+      <div class="col-8 spot-1 " style="background-color:#${item}"data-col="${item}"></div>
+      <div class="col-2 draggin d-flex align-items-center justify-content-center">
+        <span class="icon-drag_indicator"></span>
+      </div>
+    </div>
+    `
+    // console.log(typeof item)
+      b+=`#${item},`
+  })
+
+
+  spotCols.innerHTML = a
+  const spotPrev = document.getElementById('gradpreview')
+
+  let arr = spotColList.map(i => '#' + i ).join(' , ');
+
+  let len =  spotColList.length
+  spotPrev.style.transition =  'all 1s'
+  spotPrev.style.background = len === 1 ? "#"+spotColList[0] : `linear-gradient(to bottom, ${arr})`
+  spotPrev.style.height = `${spotColList.length * 38}px`
+
+  
+})
 
 
